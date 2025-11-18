4e2a2218b38571a0f4d052a85e3b39d0c6b76337
Aurèle <aurele.joblet@gmail.com>
2025-11-18 11:48:22 +0800

    setup of the application

--- electron-client/main.js
+++ electron-client/main.js
@@ -1,13 +1,27 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-function createWindow(){
+function createWindow() {
   const win = new BrowserWindow({
-    width: 900, height: 700,
-    webPreferences: { preload: path.join(__dirname, 'preload.js') }
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true
+    }
   });
-  win.loadFile(path.join(__dirname, 'renderer.html'));
+
+  win.loadFile("index.html");
 }
 
-app.whenReady().then(createWindow);
-app.on('window-all-closed', ()=>{ if(process.platform !== 'darwin') app.quit(); });
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
