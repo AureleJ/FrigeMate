4e89bfc20a3c8f3b1ca2300ced06b02bbbee2021
Aurèle <aurele.joblet@gmail.com>
2025-12-21 22:35:35 +0800

    Add API key validation middleware to enhance security

--- server/server.js
+++ server/server.js
@@ -11,6 +11,31 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
+const checkApiKey = (req, res, next) => {
+    const serverApiKey = process.env.API_KEY; 
+    console.log("Server API Key:", serverApiKey);
+
+    if (!serverApiKey) {
+        console.warn("Warning: API_KEY is not set on the server. Skipping API key check.");
+        return next();
+    }
+
+    const clientApiKey = req.headers['x-api-key'];
+    console.log("Client API Key:", clientApiKey);
+
+
+    if (!clientApiKey || clientApiKey !== serverApiKey) {
+        console.error("Unauthorized access attempt with API Key:", clientApiKey);
+        return res.status(401).json({ error: 'Unauthorized: Invalid API Key' });
+    }
+
+    console.log("API Key validated successfully.");
+
+    next();
+};
+
+app.use(checkApiKey);
+
 app.use('/users', userRoutes);
 app.use('/recipes', recipeRoutes);
 
